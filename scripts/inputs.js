bc083d1b24aba0b97e8d15babe61132968fa8f5f
Boguz <luis_bento@hotmail.com>
2019-01-12 09:21:40 +0100

    Add p1 and p2 inputs

---
+++ scripts/inputs.js
@@ -0,0 +1,24 @@
+window.addEventListener('keydown', function(e) {
+    // PLAYER ONE
+    console.log(e.keyCode);
+    if (e.keyCode === 37 && p1.snake.dir !== "right") {
+        p1.snake.dir = "left";
+    } else if (e.keyCode === 38 && p1.snake.dir !== "down") {
+        p1.snake.dir = "up";
+    } else if (e.keyCode === 39 && p1.snake.dir !== "left") {
+        p1.snake.dir = "right";
+    } else if (e.keyCode === 40 && p1.snake.dir !== "up") {
+        p1.snake.dir = "down";
+    }
+
+    // PLAYER TWO
+    if (e.keyCode === 65 && p2.snake.dir !== "right") {
+        p2.snake.dir = "left";
+    } else if (e.keyCode === 87 && p2.snake.dir !== "down") {
+        p2.snake.dir = "up";
+    } else if (e.keyCode === 68 && p2.snake.dir !== "left") {
+        p2.snake.dir = "right";
+    } else if (e.keyCode === 83 && p2.snake.dir !== "up") {
+        p2.snake.dir = "down";
+    }
+});
--- scripts/p2snake.js
+++ scripts/p2snake.js
@@ -8,7 +8,7 @@ const heartsP2 = document.querySelectorAll('.lives__container--two .lives__heart
 const scoreP1 = document.querySelector('.score__p1');
 const scoreP2 = document.querySelector('.score__p2');
 const gridSize = 15; // in px
-const frameRate = 1 // in frames per second
+const frameRate = 5 // in frames per second
 
 // VARIABLES
 let p1, p2;
@@ -35,13 +35,13 @@ function gameInit() {
                     "#0091ea",
                     0,
                     0,
-                    "down");
+                    "right");
     p2 = new Player(startingLives,
                     startingScore,
                     "#43a047",
                     canvas.width - gridSize,
                     canvas.clientHeight - gridSize,
-                    "up");
+                    "left");
 
     drawHearts(heartsP1, p1.lives);
     drawHearts(heartsP2, p2.lives);
