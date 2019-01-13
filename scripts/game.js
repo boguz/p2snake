2ece74e9251db4e4482077b0abc8c2099dd57fd9
Boguz <luis_bento@hotmail.com>
2019-01-13 10:40:57 +0100

    Rename game.js

---
+++ scripts/game.js
@@ -0,0 +1,98 @@
+// DOCUMENT CONSTANTS
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+const heartsP1 = document.querySelectorAll('.lives__container--one .lives__heart');
+const heartsP2 = document.querySelectorAll('.lives__container--two .lives__heart');
+const scoreP1 = document.querySelector('.score__p1');
+const scoreP2 = document.querySelector('.score__p2');
+
+// GAME CONSTANTS
+const startingLives = 3;
+const startingScore = 0;
+const gridSize = 15; // in px
+const cols = canvas.width / gridSize;
+const rows = canvas.height / gridSize;
+const frameRate = 5 // in frames per second
+const p1Color = "#0091ea";
+const p2Color = "#43a047";
+const p1ColorDark = "#076faf";
+const p2ColorDark = "#37773a";
+
+// VARIABLES
+let lastLoopTime = 0;
+
+// GAME CHARACTERS
+let p1, p2;
+let food;
+
+function createNewFood() {
+    return new Food();
+}
+
+function drawHearts(playerHearts, livesLeft) {
+    for (let i = 0; i < startingLives; i++) {
+        if (i <= livesLeft -1 ) {
+            playerHearts[i].classList.add('lives__heart--show');
+        } else {
+            playerHearts[i].classList.remove('lives__heart--show');
+        }
+    }
+}
+
+function eatFood(player) {
+    player.score++;
+    food = createNewFood();
+    updateScores();
+}
+
+function gameInit() {
+    p1 = new Player("p1",
+                    startingLives,
+                    startingScore,
+                    p1Color,
+                    p1ColorDark,
+                    0,
+                    0,
+                    "right",
+                    p2);
+    p2 = new Player("p2",
+                    startingLives,
+                    startingScore,
+                    p2Color,
+                    p2ColorDark,
+                    canvas.width - gridSize,
+                    canvas.clientHeight - gridSize,
+                    "left",
+                    p1);
+
+    drawHearts(heartsP1, p1.lives);
+    drawHearts(heartsP2, p2.lives);
+    updateScores();
+    p1.draw();
+    p2.draw();
+    loop(Date.now);
+    food = new Food();
+    food.draw();
+}
+
+function loop(currentTime) {
+    if (currentTime - lastLoopTime > 1000 / frameRate) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        p1.update();
+        p2.update();
+        p1.draw();
+        p2.draw();
+        detectCollisions(p1, p2);
+        detectCollisions(p2, p1);
+        food.draw();
+        lastLoopTime = currentTime;
+    }
+    requestAnimationFrame(loop);
+}
+
+function updateScores() {
+    scoreP1.textContent = p1.score;
+    scoreP2.textContent = p2.score;
+}
+
+gameInit();
